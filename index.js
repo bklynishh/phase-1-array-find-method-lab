8c37ea77297485ddbfbd22b679bf53252971d0b0
Hannah Maratea <maratea101@gmail.com>
2022-11-21 12:21:48 -0500

    commit tree a6380316c74c4439a32868b07038dd0788b96758

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin (record) {
+  const found = record.find(obj => obj.result === "W")
+  console.log(found);
+  if (found){
+    return found.year
+  }
+}
+
+//const found = array1.find(element => element > 10);
